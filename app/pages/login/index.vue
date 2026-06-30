41b9ce8f20b91b823a7b58062572c9dd82e92c82
bavleen4178 <125288915+4178bavleen@users.noreply.github.com>
2026-06-30 21:12:42 +0530

    loginPage-fix

--- app/pages/login/index.vue
+++ app/pages/login/index.vue
@@ -1,59 +1,111 @@
 <template>
   <div class="auth-wrapper-main">
-    <h1 class="auth-heading-primary">Hi, kindly sign in.</h1>
+    <h1 class="auth-heading-primary">
+      Hi, kindly sign in.
+    </h1>
+
     <div class="container">
-      <div class="row">
-        <div class="col-lg-6 offset-lg-3">
+      <div class="row justify-content-center">
+        <div class="col-12 col-sm-10 col-md-8 col-lg-6">
           <div class="auth-card-container">
-            <div class="auth-section-title">Your Account</div>
+
+            <div class="auth-section-title">
+              Your Account
+            </div>
 
             <button class="auth-social-button">
-              <img src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg" alt="Google"
-                class="auth-social-icon" />
+              <img
+                src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
+                alt="Google"
+                class="auth-social-icon"
+              />
               Continue with Google
             </button>
 
             <button class="auth-social-button">
-              <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg"
-                alt="Facebook" class="auth-social-icon" />
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg"
+                alt="Facebook"
+                class="auth-social-icon"
+              />
               Continue with Facebook
             </button>
 
-            <div class="auth-separator-line">or</div>
+            <div class="auth-separator-line">
+              or
+            </div>
 
             <form>
+
               <div class="auth-field-group">
-                <label class="auth-field-label">Email</label>
-                <input type="email" class="auth-input-field" required />
+                <label class="auth-field-label">
+                  Email
+                </label>
+
+                <input
+                  type="email"
+                  class="auth-input-field"
+                  required
+                />
               </div>
 
               <div class="auth-field-group">
-                <label class="auth-field-label">Password</label>
-                <input type="password" class="auth-input-field" required />
+                <label class="auth-field-label">
+                  Password
+                </label>
+
+                <input
+                  type="password"
+                  class="auth-input-field"
+                  required
+                />
               </div>
 
-              <button type="submit" class="auth-submit-button">
+              <button
+                type="submit"
+                class="auth-submit-button"
+              >
                 Sign In
               </button>
 
               <div class="auth-help-links">
-                <a href="#" class="auth-link-item" onclick="alert('Password reset feature coming soon')">Forgot
-                  password?</a>
-                <a href="#" class="auth-link-item"
-                  onclick="alert('Use social login or checkout to create account')">Don't have a password?</a>
-                <NuxtLink to="/checkout" class="auth-link-item">Want to create a new account?</NuxtLink>
+                <a
+                  href="#"
+                  class="auth-link-item"
+                  onclick="alert('Password reset feature coming soon')"
+                >
+                  Forgot password?
+                </a>
+
+                <a
+                  href="#"
+                  class="auth-link-item"
+                  onclick="alert('Use social login or checkout to create account')"
+                >
+                  Don't have a password?
+                </a>
+
+                <NuxtLink
+                  to="/checkout"
+                  class="auth-link-item"
+                >
+                  Want to create a new account?
+                </NuxtLink>
               </div>
+
             </form>
+
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
 <script setup>
 useHead({
   bodyAttrs: {
-    class: 'product-details-page'
+    class: "product-details-page"
   }
 })
 </script>
