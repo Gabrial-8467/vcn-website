0977c64c076b9701ee79180b7b022ccc3eee4d47
bavleen4178 <125288915+4178bavleen@users.noreply.github.com>
2026-09-21 17:53:15 +0530

    contactUs

--- app/pages/contact-us/index.vue
+++ app/pages/contact-us/index.vue
@@ -1,9 +1,6 @@
 <style>
   .terms-conditions {
-    padding:  80px;
-  }
-  .col-lg-6{
-    padding-left: 20px;
+    padding:  80px 80px;
   }
 /* ===== PILL TABS ===== */
 .office-pills-wrapper {
@@ -576,7 +573,7 @@
       <div class="row">
 
       <!-- Left Side -->
-      <div class="col-lg-6 col-12"vsc>
+      <div class="col-lg-6 col-12">
         <div class="contact-form-body">
           <div class="office-image-wrapper">
 
@@ -670,7 +667,7 @@
       </div>
 
       <!-- Right Side: Contact Form -->
-      <div class=" col-12">
+      <div class="col-lg-6 col-12">
         <div class="contact-form-container">
           <div class="contact-form-header">
             <h1 class="contact-form-title">{{ contact.contactForm.title }}</h1>
